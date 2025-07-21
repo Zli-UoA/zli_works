c5137a33a436eb84c4a088cbbf7e467c0fd877f4
39sho <g.39sho@gmail.com>
2025-07-21 15:12:51 +0900

    型エラーが出てたので修正

--- app/routes/_app._index/eventCard.tsx
+++ app/routes/_app._index/eventCard.tsx
@@ -1,5 +1,5 @@
 import { LuCalendar } from "react-icons/lu";
-import type { ConnpassEvent } from "~/.server/connpass";
+import type { ConnpassEventSchema } from "~/.server/connpass";
 import { Badge } from "~/components/ui/badge";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 
@@ -8,11 +8,11 @@ type Props = {
   ccatch: string;
   url: string;
   image: string;
-  status: ConnpassEvent["open_status"];
+  status: ConnpassEventSchema["open_status"];
   date: string | null;
 };
 
-const displayStatus = (status: ConnpassEvent["open_status"]) => {
+const displayStatus = (status: ConnpassEventSchema["open_status"]) => {
   switch (status) {
     case "preopen": {
       return <Badge>開催前</Badge>;
